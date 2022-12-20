1cf2f600787c3f6b1a636824b89ea2040759c8f9
PebbleAR <104420040+PebbleAR@users.noreply.github.com>
2022-12-20 20:06:57 +0100

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    newstring = ''
 
+    for (let i=string.length -1; i >=0; i--) {
+        newstring += string.charAt(i)
+    }
+    return newstring
 };
 
 // Do not edit below this line
