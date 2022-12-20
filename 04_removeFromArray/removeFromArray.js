fa754bfaa34b7c53b76acffa6358c8d9c884c23b
PebbleAR <104420040+PebbleAR@users.noreply.github.com>
2022-12-20 20:41:42 +0100

    Add test case, and complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let args = Array.prototype.slice.call(arguments, 0);
+    args = args.slice(1);
 
+    for (const value of args) {
+        for (let index= 0; index <= array.length -1; index++) {
+            if (value === array[index]) {
+                array.splice(index,1)
+                break
+            }
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
