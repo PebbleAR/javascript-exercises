c22642a9ff04e5479470ae77d6b999d724cf9345
PebbleAR <104420040+PebbleAR@users.noreply.github.com>
2022-12-20 20:53:50 +0100

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+total = 0
+if ((start < 0) || (end < 0) || (typeof(start) !== "number") || (typeof(end) !== "number")) {
+    return "ERROR"
+}
+if (start > end) {
+    dummy = start;
+    start = end;
+    end = dummy;
+}
+for (let i = start; i<= end; i++) {
+    total += i
+}
+return total
 };
 
 // Do not edit below this line
