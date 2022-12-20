3bfb5dad567d5bd2282f65168f62b64508974b23
PebbleAR <104420040+PebbleAR@users.noreply.github.com>
2022-12-20 20:01:18 +0100

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    returnstring = ''
+    for (let i = 1; i <= num; i++) {
+        returnstring += string;
+    }
+    return returnstring
 };
 
 // Do not edit below this line
